cb9fe934b5d5d291c2b7cbb537dfc4fc2871047a
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 04:14:16 +0500

    📦 NEW: Lesson 47 Dynamic Pages with Templating

--- src/app.js
+++ src/app.js
@@ -7,7 +7,28 @@ console.log( path.join(__dirname, '../public') );
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Andrew Mead'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Andrew Mead'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.'
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
