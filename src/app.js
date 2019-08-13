15048ea8b5517a3f7ecc38ed8168207173820126
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 00:23:20 +0500

    📦 NEW: Lesson 44 Serving up HTML and JSON

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+app.get('', (req, res) => {
+  res.send('<h1>Weather</h1>');
+});
+
+app.get('/help', (req, res) => {
+  res.send({
+    name: 'Andrew',
+    age: 27
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About</h1>');
+});
+
+app.get('/weather', (req, res) => {
+  res.send({
+    location: 'New York',
+    temperature: 28
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log( 'Server is up on port 3000' );
+});
