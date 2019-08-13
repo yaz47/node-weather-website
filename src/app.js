26f94bd956e3474eeb5540eefb1a978ea1c86697
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 04:22:25 +0500

    📦 NEW: Lesson 48 Customizing the Views Directory

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ console.log( path.join(__dirname, '../public') );
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 
+app.set('views', viewsPath);
 app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
