876fe8c78402b54366cf2e3faba79f9a143ff8a3
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 04:42:24 +0500

    📦 NEW: Lesson 49 Advanced Templating

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 console.log( __dirname );
 console.log( path.join(__dirname, '../public') );
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
 
-app.set('views', viewsPath);
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve files from
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -28,7 +36,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'This is some helpful text.'
+    title: 'Help',
+    helpText: 'This is some helpful text!',
+    name: 'Andrew Mead'
   });
 });
 
