805568c4ba324ee10cfa765240933b006210c47c
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 00:40:43 +0500

    📦 NEW: Lesson 45 Serving up Static Assets

--- src/app.js
+++ src/app.js
@@ -1,21 +1,13 @@
+const path = require('path');
 const express = require('express');
 
+console.log( __dirname );
+console.log( path.join(__dirname, '../public') );
+
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>');
-});
-
-app.get('/help', (req, res) => {
-  res.send({
-    name: 'Andrew',
-    age: 27
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About</h1>');
-});
+app.use(express.static(publicDirectoryPath));
 
 app.get('/weather', (req, res) => {
   res.send({
@@ -23,8 +15,6 @@ app.get('/weather', (req, res) => {
     temperature: 28
   });
 });
-
-
 
 app.listen(3000, () => {
   console.log( 'Server is up on port 3000' );
