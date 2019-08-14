1c8f2cafbfacc7726e4ab0570ed636cc69d643f3
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 23:59:17 +0500

    📦 NEW: Lesson 56 ES6 Aside Default Function Parameters

--- src/app.js
+++ src/app.js
@@ -48,19 +48,19 @@ app.get('/weather', (req, res) => {
     });
   }
 
-  geocode(req.query.address, (error, data) => {
+  geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
     if (error) {
       return res.send({ error });
     }
 
-    forecast(data, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         return res.send({ error });
       }
 
       res.send({
         forecast: forecastData,
-        location: data.location,
+        location,
         address: req.query.address
       });
     });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const forecast = ({latitude, longitude}, callback) => {
+const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/8f79b6db4e34c587493778e6d0b9276e/${latitude},${longitude}?units=si`;
 
   request({ url, json: true}, (error, {body}) => {
