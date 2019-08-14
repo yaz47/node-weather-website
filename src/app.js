339f03d3c3f6491e88cc0e3dd4eb7acdc2c2f26f
yaz_uaz <zayarnyy74@gmail.com>
2019-08-14 22:57:27 +0500

    📦 NEW: Lesson 54 THe Query String

--- src/app.js
+++ src/app.js
@@ -43,11 +43,32 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!'
+    });
+  }
+
   res.send({
-    location: 'New York',
-    temperature: 28
+    forecast: 'It is snowing',
+    location: 'Philadelphia',
+    address: req.query.address
   });
 });
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    });
+  }
+
+  console.log( req.query );
+  res.send({
+    products: []
+  });
+});
+
 
 app.get('/help/*', (req, res) => {
   res.render('404', {
