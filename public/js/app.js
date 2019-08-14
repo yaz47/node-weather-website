fb0f766c62722565bfd3d507d0b74606ed19f6ee
yaz_uaz <zayarnyy74@gmail.com>
2019-08-15 01:57:54 +0500

    📦 NEW: Lesson 59 Wiring up the User Interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,28 @@
 console.log('Client side javascript file loaded');
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log( data.error );
-    } else {
-      console.log( data.location );
-      console.log( data.forecast );
-    }
-  });
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.textContent = '';
+messageTwo.textContent = '';
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const location = search.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
   response.json().then((data) => {
     if (data.error) {
-      console.log( data.error );
+      messageOne.textContent = data.error;
+      messageTwo.textContent = '';
     } else {
-      console.log( data.location );
-      console.log( data.forecast );
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
     }
   });
 });
